a8639aeda04ee928441ef2478d2edb3b763499d7
Jays2Kings <jays@outlook.com>
2023-04-19 18:13:30 -0400

    Version 1.7.0-b02

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,7 +4,7 @@ object AndroidVersions {
     const val compileSdk = 33
     const val minSdk = 23
     const val targetSdk = 33
-    const val versionCode = 104
+    const val versionCode = 105
     const val versionName = "1.7.0"
     const val ndk = "23.1.7779620"
     const val kotlin = "1.8.10"

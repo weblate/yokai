7f41c5481227ca150207aeb99a2aa17e1a0bcade
ziro <palembani@gmail.com>
2024-02-04 11:08:12 +0700

    chore: Adjust enum positioning

--- app/src/main/java/dev/yokai/domain/base/BasePreferences.kt
+++ app/src/main/java/dev/yokai/domain/base/BasePreferences.kt
@@ -10,9 +10,9 @@ class BasePreferences(private val preferenceStore: PreferenceStore) {
     fun extensionInstaller() = preferenceStore.getEnum("extension_installer", ExtensionInstaller.PACKAGEINSTALLER)
 
     enum class ExtensionInstaller(@StringRes val titleResId: Int, val requiresSystemPermission: Boolean) {
-        LEGACY(R.string.ext_installer_legacy, true),  // Technically useless, but just in case it being missing crashes the app
         PACKAGEINSTALLER(R.string.ext_installer_packageinstaller, true),
         SHIZUKU(R.string.ext_installer_shizuku, false),
         PRIVATE(R.string.ext_installer_private, false),
+        LEGACY(R.string.ext_installer_legacy, true),  // Technically useless, but just in case it being missing crashes the app
     }
 }

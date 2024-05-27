6887d779efd87b5711508eb44d35643135776a64
Tachiyomi Maintainer <maintainer@tachiyomi.org>
2024-05-27 07:24:13 +0700

    feat: Composable preference widgets

--- app/src/main/java/dev/yokai/presentation/core/util/ModifierExtensions.kt
+++ app/src/main/java/dev/yokai/presentation/core/util/ModifierExtensions.kt
@@ -1,7 +1,20 @@
 package dev.yokai.presentation.core.util
 
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import dev.yokai.presentation.theme.SecondaryItemAlpha
 
 fun Modifier.secondaryItemAlpha(): Modifier = this.alpha(SecondaryItemAlpha)
+
+fun Modifier.clickableNoIndication(
+    interactionSource: MutableInteractionSource,
+    onLongClick: (() -> Unit)? = null,
+    onClick: () -> Unit,
+) = this.combinedClickable(
+    interactionSource = interactionSource,
+    indication = null,
+    onLongClick = onLongClick,
+    onClick = onClick,
+)

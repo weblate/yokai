9e0f94b573644b18783d3cf18f8fdff3a0108db1
Ahmad Ansori Palembani <palembani@gmail.com>
2024-06-04 07:04:00 +0700

    refactor: Make StorIO consistent with SQLDelight

--- app/src/main/java/eu/kanade/tachiyomi/data/database/tables/HistoryTable.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/database/tables/HistoryTable.kt
@@ -10,40 +10,21 @@ object HistoryTable {
     /**
      * Id column name
      */
-    const val COL_ID = "${TABLE}_id"
+    const val COL_ID = "history_id"
 
     /**
      * Chapter id column name
      */
-    const val COL_CHAPTER_ID = "${TABLE}_chapter_id"
+    const val COL_CHAPTER_ID = "history_chapter_id"
 
     /**
      * Last read column name
      */
-    const val COL_LAST_READ = "${TABLE}_last_read"
+    const val COL_LAST_READ = "history_last_read"
 
     /**
      * Time read column name
      */
-    const val COL_TIME_READ = "${TABLE}_time_read"
+    const val COL_TIME_READ = "history_time_read"
 
-    /**
-     * query to create history table
-     */
-    val createTableQuery: String
-        get() =
-            """CREATE TABLE $TABLE(
-            $COL_ID INTEGER NOT NULL PRIMARY KEY,
-            $COL_CHAPTER_ID INTEGER NOT NULL UNIQUE,
-            $COL_LAST_READ LONG,
-            $COL_TIME_READ LONG,
-            FOREIGN KEY($COL_CHAPTER_ID) REFERENCES ${ChapterTable.TABLE} (${ChapterTable.COL_ID})
-            ON DELETE CASCADE
-            )"""
-
-    /**
-     * query to index history chapter id
-     */
-    val createChapterIdIndexQuery: String
-        get() = "CREATE INDEX ${TABLE}_${COL_CHAPTER_ID}_index ON $TABLE($COL_CHAPTER_ID)"
 }

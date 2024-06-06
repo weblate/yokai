d0baff197cd7e4353f78f95553fbd330b19914f0
Ahmad Ansori Palembani <palembani@gmail.com>
2024-06-06 12:32:57 +0700

    refactor: Remove Material 2

--- app/src/main/java/dev/yokai/presentation/component/preference/widget/MultiListPreferenceWidget.kt
+++ app/src/main/java/dev/yokai/presentation/component/preference/widget/MultiListPreferenceWidget.kt
@@ -1,6 +1,6 @@
 package dev.yokai.presentation.component.preference.widget
 
-import androidx.compose.material.AlertDialog
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable

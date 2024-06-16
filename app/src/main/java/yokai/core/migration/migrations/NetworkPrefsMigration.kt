4d8643b4df1e2dda19a8cb86ffa07355ed785cbc
Ahmad Ansori Palembani <palembani@gmail.com>
2024-06-16 12:17:50 +0700

    debug: Debug prints

--- app/src/main/java/yokai/core/migration/migrations/NetworkPrefsMigration.kt
+++ app/src/main/java/yokai/core/migration/migrations/NetworkPrefsMigration.kt
@@ -1,6 +1,7 @@
 package yokai.core.migration.migrations
 
 import androidx.preference.PreferenceManager
+import co.touchlab.kermit.Logger
 import eu.kanade.tachiyomi.App
 import eu.kanade.tachiyomi.network.NetworkPreferences
 import yokai.core.migration.Migration
@@ -11,13 +12,20 @@ class NetworkPrefsMigration : Migration {
 
     override suspend fun invoke(migrationContext: MigrationContext): Boolean {
         val context: App = migrationContext.get() ?: return false
+        Logger.d { "Got context" }
         val preferences: NetworkPreferences = migrationContext.get() ?: return false
+        Logger.d { "Got networkPref" }
         val prefs = PreferenceManager.getDefaultSharedPreferences(context)
+        Logger.d { "Got sharedPref" }
 
         val dohProvider = prefs.getInt("doh_provider", -1)
+        Logger.d { "Got dohProvider" }
         if (dohProvider > -1) {
+            Logger.d { "Migrating" }
             preferences.dohProvider().set(dohProvider)
+            Logger.d { "Migrated" }
         }
+        Logger.d { "Done" }
         return true
     }
 }

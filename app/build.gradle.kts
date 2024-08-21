399c532cd243d41a4c2447a6b8ab336cfecc8465
Ahmad Ansori Palembani <palembani@gmail.com>
2024-08-21 18:22:43 +0700

    chore(release): v1.8.5.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ fun runCommand(command: String): String {
     return String(byteOut.toByteArray()).trim()
 }
 
-val _versionName = "1.8.5.3"
+val _versionName = "1.8.5.4"
 val betaCount by lazy {
     val betaTags = runCommand("git tag -l --sort=refname v${_versionName}-b*")
 
@@ -54,7 +54,7 @@ val supportedAbis = setOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
 android {
     defaultConfig {
         applicationId = "eu.kanade.tachiyomi"
-        versionCode = 144
+        versionCode = 145
         versionName = _versionName
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true

a6f697199d9a8b8cb746fd7adeb723de0ad63ebc
Ahmad Ansori Palembani <palembani@gmail.com>
2024-09-26 13:34:13 +0700

    revert: "Explicitly bind app to Application class"
    This reverts commit a8b3d97b1496e1a94f447fe0ac5988f724cf7bc5.

--- app/src/main/java/yokai/core/di/AppModule.kt
+++ app/src/main/java/yokai/core/di/AppModule.kt
@@ -1,7 +1,6 @@
 package yokai.core.di
 
 import android.app.Application
-import android.content.Context
 import androidx.sqlite.db.SupportSQLiteOpenHelper
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
@@ -37,8 +36,7 @@ import yokai.domain.SplashState
 import yokai.domain.storage.StorageManager
 
 fun appModule(app: Application) = module {
-    single<Application> { app }
-    single<Context> { app }
+    single { app }
 
     single<SupportSQLiteOpenHelper> {
         val configuration = SupportSQLiteOpenHelper.Configuration.builder(app)

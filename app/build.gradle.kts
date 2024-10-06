4b46e9a02c5456b9013ed4a6ab7c9840d2832653
Ahmad Ansori Palembani <palembani@gmail.com>
2024-10-06 16:10:59 +0700

    chore(release): v1.8.5.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ fun runCommand(command: String): String {
     return String(byteOut.toByteArray()).trim()
 }
 
-val _versionName = "1.8.5.8"
+val _versionName = "1.8.5.9"
 val betaCount by lazy {
     val betaTags = runCommand("git tag -l --sort=refname v${_versionName}-b*")
 

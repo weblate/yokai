fe2bdc3846aea955c313b841fd60a80db546a3e9
Ahmad Ansori Palembani <palembani@gmail.com>
2024-10-12 08:13:45 +0700

    chore: Clean up log

--- app/src/main/java/eu/kanade/tachiyomi/data/backup/create/BackupCreator.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/backup/create/BackupCreator.kt
@@ -107,7 +107,7 @@ class BackupCreator(
 
             return fileUri.toString()
         } catch (e: Exception) {
-            Logger.e(e) { if (file == null) "Unable to retrieve backup destination" else "Invalid backup destination" }
+            Logger.e(e) { "Backup failed: ${e.message}" }
             file?.delete()
             throw e
         }

dceac33b7ce51b4e965dce30b2db834616562262
Ahmad Ansori Palembani <palembani@gmail.com>
2024-11-14 09:13:21 +0700

    chore: Testing setToForeground on auto backup (again)

--- app/src/main/java/eu/kanade/tachiyomi/data/backup/create/BackupCreatorJob.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/backup/create/BackupCreatorJob.kt
@@ -48,12 +48,15 @@ class BackupCreatorJob(private val context: Context, workerParams: WorkerParamet
             ?: getAutomaticBackupLocation()
             ?: return Result.failure()
 
+        tryToSetForeground()
+        /*
         if (isAutoBackup) {
             notifier.showBackupProgress()  // Remove when tryToSetForeground is "fixed"
         } else {
             // FIXME: tryToSetForeground keep causing auto backup to fail
             tryToSetForeground()
         }
+        */
 
         val options = inputData.getBooleanArray(BACKUP_FLAGS_KEY)?.let { BackupOptions.fromBooleanArray(it) }
             ?: BackupOptions()

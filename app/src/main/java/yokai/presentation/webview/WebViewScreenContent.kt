0c6f86c1aed2983661f43d4f8e0c040a10bbff58
Ahmad Ansori Palembani <palembani@gmail.com>
2024-11-26 23:04:13 +0700

    fix(webview): Add imePadding

--- app/src/main/java/yokai/presentation/webview/WebViewScreenContent.kt
+++ app/src/main/java/yokai/presentation/webview/WebViewScreenContent.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
@@ -229,7 +230,8 @@ fun WebViewScreenContent(
             state = state,
             modifier = Modifier
                 .fillMaxSize()
-                .padding(contentPadding),
+                .padding(contentPadding)
+                .imePadding(),
             navigator = navigator,
             onCreated = { webView ->
                 webView.setDefaultSettings()

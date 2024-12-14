93e5effaaf32121a8cc6498bcaecbef643dd1105
Ahmad Ansori Palembani <palembani@gmail.com>
2024-12-14 19:55:09 +0700

    chore: Bump version to 1.9.5 for nightly and beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ fun runCommand(command: String): String {
     return String(byteOut.toByteArray()).trim()
 }
 
-val _versionName = "1.9.4"
+val _versionName = "1.9.5"
 val betaCount by lazy {
     val betaTags = runCommand("git tag -l --sort=refname v${_versionName}-b*")
 
